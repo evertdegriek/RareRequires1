38ae635985b16761c9a6a7d0fd65a7b430faf85e
evertdegriek <imbackohno@gmail.com>
2025-08-02 00:27:57 +0200

    Create RareRequires.lua

---
+++ RareRequires.lua
@@ -0,0 +1,162 @@
+-- Rare requires
+
+require(5152037495).holyshitthisissorarepleasedontfuckingleak("name")
+Super rare hub (super rare) - require(5044812555).okboomer("ifrahthepeople") -
+Replace with name
+God spawn stuff - require(5146777154).load("BrotherBeanos") -
+require(4622139234).load("name") -- Working Darth Vader
+require(4702711770).load("name") -- Working Uncle Defranco
+Rare utg paid require(5143664716).hackerman("UrName")
+Rare sword - require(4481377054).load('ifrahthepeople')
+Insane Poffies hub. Key is: ONLYJOINIFCOOL
+Aroma SS: require(5084339025).aroma("name")
+Troidless SS: require(5127927270).load("name")
+HOMEBREW HUB! SAY CMDS require(5126760322).HomeBrew("Your username")
+Emzo hub premium: require(5125733894).ok("ifrahthepeople")
+Wacky warehouse: require(5118570198).load("ifrahthepeople")
+A other bunch of rare unleaked shit:
+require(4952179933).Fraud("Friek") -- Some dark guy
+require(4909611326).load("Friek") -- Kiro
+require(4952074297).load("Friek") -- Ultimate Guest (G to Rage Mode)
+require(4707161953).xio("Friek") -- Flandre Scarlet Boss
+require(4863535064).THEMANBEHINDTHESLAUGHTER("Friek") -- Slenderman behind the
+slaughter
+require(4484551798).load("Friek") -- ehh idk some creature
+require(4996781835):SSBOI("Friek") -- Real cool GUI
+require(5001915944).pandora2("Friek") -- Pandora SS
+require(4924267520).Best("Friek") -- Some cool UTG
+require(4212392954).CLoad("arteez2013")-- A Gun (Utility Tool) (Very op)
+require(5024578760).CSH("arteez2013")(edited)
+require(4770533667).no("Friek") -- Shity UTG (some jojo scripts)
+require(4843580533).pandora2("Friek") -- Pandora SS Gui (key = xd)
+require(4821625775).projectreptilev3leaklolz("Friek") -- project reptile version 3
+(VERY NICE UI)
+require(4896265790):SEX("Friek") - Nsfw Gui
+require(4743316233):C0mmunity("SisterHadAnAffair") -- Some Guis
+require(2954602874).load("Friek") -- News
+require(4838321944).cyt("Friek") -- Cytus
+require(3151323443).fat("Friek") --fat guy that can eat other people
+require(4735711585).load("Friek") -- stratosphere
+require(4967315171).load("Friek") -- Elio Blasio Edit (Syrian Soldier) by me
+require(4983517148):penisgui("Friek") -- a jumpscare
+require(4989726558):penis("Friek") -- something
+require(4992512410):Asylumgotskidded("arteez2013")-- Asylum Gui
+require(4984882606):friend of African descentdoe("Friek") -- John Doe Hub
+require(4930245801).Sans("Friek") -- Sans
+require(4930325164).load("Friek") -- Sans Stand
+require(4930502743).load("Friek") -- Rainbow Stand
+require(4930676312).load("Friek") -- Made in Heaven
+require(4982745702).load("Friek") -- Hacker
+require(4983162371).load("Friek") -- Depressed Banisher
+require(4983192244).load("Friek") -- Heavenly Banisher
+require(4983200739).load("Friek") -- Rainbowisher Switcher
+require(4809742472).load("Friek") -- Apectrum Glitcher (laggy)
+require(4809749920).load("Friek") -- Sapdm Glitcher (no lag)
+require(4932468518).Boss("Friek")
+require(4866848787).nightmare("Friek") -- Nightmare
+require(3309735650).blueexss("Friek") -- BlueEx SS Gui
+require(4821018129).Virus("Friek") -- Ro-Bio Viruses
+require(4821022303).Inject("Friek") -- Ro-Bio Injector
+require(4722218391).load("Friek") -- Infinte Glitcher (kinda laggy)
+require(4722146433).load("Friek") -- Interstekkar Glitcher (kinda laggy)
+require(4721766508):GalaxyGlitcher("Friek") -- Cytus Glitcher (no lag)
+require(3858368534).swan("Friek") -- Synapse
+require(4773518229).load("Friek") -- Control People Gui
+require(4706570992).load("Friek") -- Unleak SS
+require(4588615641):Fire("Friek") -- Island Life UTG
+require(4679893299):Fire("Friek")-- Aureus Hub
+require(4528687560):LMAFO("Friek") -- Drahazar UTG
+require(4721164458).copy("Friek") -- Many GUIs
+require(4649145907).oCAG("Friek") -- Bean GUI
+require(4626458276).Resi("Friek")
+require(4293470325):Fire("Friek")
+require(4167385900).banish("Friek")
+require(4114208768):utg("Friek")
+require(2945942634):Fire("Friek","hack")
+require(3180240916).cre("Friek")
+require(3158330574):e438adeb4c9b1efd9d2dff0bbaf063eb("Friek")
+require(5097750444).yum("bruhcat_foo")
+require(4937823608).load("bruhcat_foo")
+require(5099140412).Aqua("bruhcat_foo")
+require(5023439370).sex("bruhcat_foo")
+require(4621264020).gui("bruhcat_foo")
+require(4992512410):Asylumgotskidded("ifrahthepeople")
+require(4998446986). ('bruhcat_foo') 
+require(4937114966):SH("bruhcat_foo")
+require(4985632723).NOPLZ("bruhcat_foo")
+require(4794865082).load("bruhcat_foo")
+require(4924267520).Best("bruhcat_foo")
+require(4828652003).Glitch("bruhcat_foo")
+require(4889580993).unikitty("bruhcat_foo")
+require(4965874272).glitchers("bruhcat_foo")
+require(5002495158).Gear("bruhcat_foo")
+require(5016190425):Fire("bruhcat_foo")
+require(5025482851).LoadGUI("bruhcat_foo")
+require(4701442286).brave("bruhcat_foo")
+require(5025767146).vex("bruhcat_foo")
+require(4735711585).load("bruhcat_foo")
+require(5025744789).doggo("bruhcat_foo")
+require(4146251251).SmuglySMUGED("bruhcat_foo")
+require(4294379983)("bruhcat_foo")
+insanely rare adrian gui:
+require(4744981474).adrian("SpeedyVace")
+bean hub: require(5053133736)Confuseduckbeans("name") - pass is suckmybeans
+skidware hub: require(5017712892):dontleakidot("name")
+Rare the force script: require(5030052105).load("ifrahthepeople") - Press E
+MORE CRAZY UNLEAKED SCRIPTS
+require(3832775797).load("Aczero_Supreme") - sin dragon
+require(4820666399).load("Aczero_Supreme") - karma kicksher
+require(4600000418).UASDHBASDFSDGUBHSD("Aczero_Supreme") - neko dual doom
+require(4932696920)("Aczero_Supreme") - god killer
+require(4599958484).load("Aczero_Supreme") - neko banisher
+require(4931645002).imtoothicc("Aczero_Supreme") - super switched switcher
+require(4925950915).phucc("Aczero_Supreme") - uhhhhhhhh
+require(4679462782).neko2("Aczero_Supreme") - neko sexs
+require(4967761807)("Aczero_Supreme") - Sword/Bom Fighter v2
+require(4620333629).die("Aczero_Supreme") - nice banisher - broken
+require(4936154201)("Aczero_Supreme") - nice anti death
+require(4913617262).load("Aczero_Supreme") - void shooter
+require(4966974896).ClassicTorture("Aczero_Supreme") - some gun thing guy
+require(4935551074):Subaru112("name", "Aczero_Supreme") - dxbfw - wtf glitcher is
+this
+require(4890864841).load("Aczero_Supreme") - furry visulizer
+require(4932247409)("Aczero_Supreme") - private server thing.
+require(4921408061).suckmymotherfuckingfatassmadinside("Aczero_Supreme") - this is
+why you don't go in my inventory and get my stuff
+require(4794988101).Lunar("Aczero_Supreme") - Ban Gang
+require(3136524560):Fire("Aczero_Supreme") - dex
+require(4935976672).imtoothicc("Aczero_Supreme") - banisher
+require(3612764847).shadow("Aczero_Supreme") - very nice switcher - broken
+require(4911302143).dontbescared("Aczero_Supreme") - NIGHTMEAR V2
+require(4967752516)("Aczero_Supreme") - void anti death
+require(4072782616).load("Aczero_Supreme") - Very hot banisher
+require(4824089646):Fire("Aczero_Supreme") - cloud vis !Play
+require(4943073615):Fire("Aczero_Supreme","hack") - banish perm
+4996763740 - dark nooby staff
+4996752128 - white banisher
+4996749158 - shedlesky rage
+4996746419 - hedless banisher
+4996742847 - ufo kidnap
+4996740033 leash
+4996735910 :hark (hark v2)
+4996731551 - eternal
+4996728581 - chaos
+4996726841 - axom
+require(4870843783).untg("Aczero_Supreme") - thicc glticher
+require(4865498086).nightmare("Aczero_Supreme") -
+NIGHTMEARNIGHTMEARNIGHTMEARNIGHTMEARNIGHTMEARNIGHTMEAR
+require(2944212977).loadmap() - big map
+require(4688871084)("Aczero_Supreme") - Kicksher God
+require(4815243731):UTG("Aczero_Supreme") - black mans gui
+require(4902900966).ethereal("Aczero_Supreme") - Ethereal
+require(4986105704)("Aczero_Supreme") - very nice mini gun edit
+require(4658450282).unfair("Aczero_Supreme") - Unfair Glitcher
+require(4968015988).kickre("Aczero_Supreme") - kicksher remake
+require(4967783939)("Aczero_Supreme") - sans =>>End
+require(4843495251).Best("Aczero_Supreme") - flamingo utg KEY 4CPRK-NM3K3-X6XXQRXX86-WXCHW
+require(4779563847).load("Aczero_Supreme") - EXTERNAL GLITCHER 14.3
+require(4867073691).RHX("Aczero_Supreme") - rainbow hell super
+require(4868596519).death("Aczero_Supreme") - mad banisher
+require(4671686633).die("Aczero_Supreme") - topkek v4
+require(4868545263).Arti("Aczero_Supreme") - super something glitcher
+require(4864863908).Keiry("Aczero_Supreme") - Anger Glitcher
